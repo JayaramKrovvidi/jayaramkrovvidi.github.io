dcf032fa325226142c9fcdeb00330b5fd1ec33f3
Jayaram Krovvidi <jayaram.krovvidi@outlook.com>
2023-08-25 11:18:18 -0400

    Initialized Routing and angular theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'me', pathMatch: 'full' },
+  { path: 'me', component: PortfolioComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
