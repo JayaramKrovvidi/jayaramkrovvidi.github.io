4aa46fd02f5559258785ad34af4f385bc6998588
Jayaram Krovvidi <jayaram.krovvidi@outlook.com>
2023-09-07 20:32:06 -0400

    Fixed Background scaling for mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,11 @@ export class AppComponent implements AfterViewInit {
       mouseControls: true,
       touchControls: true,
       gyroControls: false,
-      minHeight: 200.00,
+      minHeight: 800.00,
       minWidth: 200.00,
       scale: 1.00,
-      scaleMobile: 1.00
+      scaleMobile: 1.00,
+      points: window.innerWidth < 576 ? 6 : 10
     })
   }
 
